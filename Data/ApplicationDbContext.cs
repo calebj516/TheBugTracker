34bb694cfc7bf9d557361dc951363f8275496805
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-12-08 23:15:36 -0600

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TheBugTracker.Models;
+
+namespace TheBugTracker.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<BTUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
