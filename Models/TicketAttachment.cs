dddd66fd36ecbd4ce66b4b2990248dfacec5762a
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-12-09 21:31:13 -0600

    Data Models - TicketAttachment

---
+++ Models/TicketAttachment.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Sockets;
+
+namespace TheBugTracker.Models
+{
+    public class TicketAttachment
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+        [DisplayName("File Date")]
+        public DateTimeOffset Created { get; set; }
+
+        [DisplayName("Team Member")]
+        public string? UserId { get; set; }
+
+        [DisplayName("File Description")]
+        public string? Description { get; set; }
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile? FormFile { get; set; }
+
+        [DisplayName("File Name")]
+        public string? FileName { get; set; }
+        public byte[]? FileData { get; set; }
+
+        [DisplayName("File Extension")]
+        public string? FileContentType { get; set; }
+
+        // Navigation Properties (allows us to specify the relationship of one class to another)
+        public virtual Ticket Ticket { get; set; }
+        public virtual BTUser User { get; set; }
+    }
+}
