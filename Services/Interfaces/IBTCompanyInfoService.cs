6b987208c17b8915b6950262a40b83bf1d2f7fdb
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-12-23 18:17:11 -0600

    Services - Company Info Service (Part 1)
    Created an interface to use with the company info service implementation.

---
+++ Services/Interfaces/IBTCompanyInfoService.cs
@@ -0,0 +1,16 @@
+﻿using TheBugTracker.Models;
+
+namespace TheBugTracker.Services.Interfaces
+{
+    public interface IBTCompanyInfoService
+    {
+        public Task<Company> GetCompanyInfobyIdAsync(int? companyId);
+
+        public Task<List<BTUser>> GetAllMembersAsync(int companyId);
+
+        public Task<List<Project>> GetAllProjectsAsync(int companyId);
+
+        public Task<List<Ticket>> GetAllTicketsAsync(int companyId);
+
+    }
+}
