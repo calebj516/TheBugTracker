7b8cac01b7fdf5fac7879fb95b2e938119e939a8
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-12-26 16:02:17 -0600

    Services - Project Service (Part 2)
    Implemented several more methods from the interface

--- Services/BTProjectService.cs
+++ Services/BTProjectService.cs
@@ -14,6 +14,7 @@ namespace TheBugTracker.Services
             _context = context;
         }
 
+        // CRUD - Create
         public async Task AddNewProjectAsync(Project project)
         {
             _context.Add(project);
@@ -30,6 +31,7 @@ namespace TheBugTracker.Services
             throw new NotImplementedException();
         }
 
+        // CRUD - Archive (Delete)
         public async Task ArchiveProjectAsync(Project project)
         {
             project.Archived = true;
@@ -44,17 +46,47 @@ namespace TheBugTracker.Services
 
         public async Task<List<Project>> GetAllProjectsByCompany(int companyId)
         {
-            throw new NotImplementedException();
+            List<Project> projects = new();
+
+            projects = await _context.Projects.Where(p => p.CompanyId == companyId)
+                                            .Include(p => p.Members)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.Comments)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.Attachments)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.History)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.DeveloperUser)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.OwnerUser)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.Notifications)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.TicketStatus)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.TicketPriority)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.TicketType)
+                                            .Include(p => p.ProjectPriority)
+                                            .ToListAsync();
+
+            return projects;
         }
 
         public async Task<List<Project>> GetAllProjectsByPriority(int companyId, string priorityName)
         {
-            throw new NotImplementedException();
+            List<Project> projects = await GetAllProjectsByCompany(companyId);
+            int priorityId = await LookupProjectPriorityId(priorityName);
+
+            return projects.Where(p => p.ProjectPriorityId == priorityId).ToList();
         }
 
         public async Task<List<Project>> GetArchivedProjectsByCompany(int companyId)
         {
-            throw new NotImplementedException();
+            List<Project> projects = await GetAllProjectsByCompany(companyId);
+
+            return projects.Where(p => p.Archived == true).ToList();
         }
 
         public async Task<List<BTUser>> GetDevelopersOnProjectAsync(int projectId)
@@ -62,6 +94,7 @@ namespace TheBugTracker.Services
             throw new NotImplementedException();
         }
 
+        // CRUD - Read
         public async Task<Project> GetProjectByIdAsync(int projectId, int companyId)
         {
             Project? project = await _context.Projects
@@ -105,7 +138,8 @@ namespace TheBugTracker.Services
 
         public async Task<int> LookupProjectPriorityId(string priorityName)
         {
-            throw new NotImplementedException();
+            int priorityId = (await _context.ProjectPriorities.FirstOrDefaultAsync(p => p.Name == priorityName)).Id;
+            return priorityId;
         }
 
         public async Task RemoveProjectManagerAsync(int projectId)
@@ -123,6 +157,7 @@ namespace TheBugTracker.Services
             throw new NotImplementedException();
         }
 
+        // CRUD - Update
         public async Task UpdateProjectAsync(Project project)
         {
             _context.Update(project);
