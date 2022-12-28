4f2a8c2059e7cb321083ef0fb58eb116f3c36b55
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-12-27 19:21:32 -0600

    Services - Ticket Service (Part 2)
    Completed the lookup methods

--- Services/BTTicketService.cs
+++ Services/BTTicketService.cs
@@ -103,17 +103,41 @@ namespace TheBugTracker.Services
 
         public async Task<int?> LookupTicketPriorityIdAsync(string priorityName)
         {
-            throw new NotImplementedException();
+            try
+            {
+                TicketPriority? priority = await _context.TicketPriorities.FirstOrDefaultAsync(p => p.Name == priorityName);
+                return priority?.Id;
+            }
+            catch
+            {
+                throw;
+            }
         }
 
         public async Task<int?> LookupTicketStatusIdAsync(string statusName)
         {
-            throw new NotImplementedException();
+            try
+            {
+                TicketStatus? status = await _context.TicketStatuses.FirstOrDefaultAsync(s => s.Name == statusName);
+                return status?.Id;
+            }
+            catch
+            {
+                throw;
+            }
         }
 
         public async Task<int?> LookupTicketTypeIdAsync(string typeName)
         {
-            throw new NotImplementedException();
+            try
+            {
+                TicketType? type = await _context.TicketTypes.FirstOrDefaultAsync(t => t.Name == typeName);
+                return type?.Id;
+            }
+            catch
+            {
+                throw;
+            }
         }
 
         public async Task UpdateTicketAsync(Ticket ticket)
