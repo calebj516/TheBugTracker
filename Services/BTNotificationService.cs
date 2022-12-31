a04ec83f31f1453afec975f76bdb6d6108598c46
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-12-30 20:38:36 -0600

    Services - Notification Service (Part 1a)
    Created an interface, scaffolded out a basic implementation of the interface in a class, and completed two of six methods.

---
+++ Services/BTNotificationService.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+using TheBugTracker.Data;
+using TheBugTracker.Models;
+using TheBugTracker.Services.Interfaces;
+
+namespace TheBugTracker.Services
+{
+    public class BTNotificationService : IBTNotificationService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IEmailSender _emailSender;
+        private readonly IBTRolesService _rolesService;
+
+        public BTNotificationService(ApplicationDbContext context, IEmailSender emailSender, IBTRolesService rolesService)
+        {
+            _context = context;
+            _emailSender = emailSender;
+            _rolesService = rolesService;
+        }
+
+        public async Task AddNotificationAsync(Notification notification)
+        {
+            try
+            {
+                await _context.AddAsync(notification);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<List<Notification>> GetReceivedNotificationsAsync(string userId)
+        {
+            try
+            {
+                List<Notification> notifications = await _context.Notifications
+                                                                 .Include(n => n.Recipient)
+                                                                 .Include(n => n.Sender)
+                                                                 .Include(n => n.Ticket)
+                                                                    .ThenInclude(t => t.Project)
+                                                                 .Where(n => n.RecipientId == userId)
+                                                                 .ToListAsync();
+                return notifications;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public Task<List<Notification>> GetSentNotificationsAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendEmailNotificationAsync(Notification notification, string emailSubject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendEmailNotificationsByRoleAsync(Notification notification, int companyId, string role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendMembersEmailNotificationsAsync(Notification notification, List<BTUser> members)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
