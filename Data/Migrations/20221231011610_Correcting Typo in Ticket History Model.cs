1ca487ca564d0105f879a135ab003a85d4f32c4b
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2022-12-30 19:19:29 -0600

    Misc - Typo Correction
    Performed another migration after correcting a typo in the Ticket History model ("Descripton" to "Description")

---
+++ Data/Migrations/20221231011610_Correcting Typo in Ticket History Model.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheBugTracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrectingTypoinTicketHistoryModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Descripton",
+                table: "TicketHistories",
+                newName: "Description");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "TicketHistories",
+                newName: "Descripton");
+        }
+    }
+}
