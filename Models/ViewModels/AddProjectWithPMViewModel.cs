c918a0c8f235a25713526594beebdad4279e9eec
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-01-06 20:41:05 -0600

    Project Features - Add New Project GET Method
    Built out the GET version of the Project controller's Create action with the aid of a new View Model, AddProjectWIthPMViewModel

---
+++ Models/ViewModels/AddProjectWithPMViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TheBugTracker.Models.ViewModels
+{
+    public class AddProjectWithPMViewModel
+    {
+        public Project? Project { get; set; }
+
+        public SelectList? PMList { get; set; }
+
+        public string? PmId { get; set; }
+
+        public SelectList? PriorityList { get; set; }
+
+        public int ProjectPriority { get; set; }
+    }
+}
