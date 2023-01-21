91f70400a83ea6aa2921f424be8ddd541307a82c
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-01-20 20:38:13 -0600

    Project Management - Assign PM - GET Method
    Created AssignPM Action along with a View Model to supply the data it will need in order to function properly

---
+++ Models/ViewModels/AssignPMViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TheBugTracker.Models.ViewModels
+{
+    public class AssignPMViewModel
+    {
+        public Project? Project { get; set; }
+        public SelectList? PMList { get; set; }
+        public int PMID { get; set; }
+    }
+}
