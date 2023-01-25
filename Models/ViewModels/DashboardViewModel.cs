afc27e757f595ea7da4c87992c8dbce0843849ac
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-01-24 19:31:49 -0600

    Dashboard - GET Method & ViewModel
    Created a Dashboard Action for the Home Controller, as well as a new ViewModel to provide the information it (and eventually the View in upcoming commits) needs to function

---
+++ Models/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TheBugTracker.Models.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public Company? Company { get; set; }
+
+        public List<Project>? Projects { get; set; }
+
+        public List<Ticket>? Tickets { get; set; }
+
+        public List<BTUser>? Members { get; set; }
+    }
+}
