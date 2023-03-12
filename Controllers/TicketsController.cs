f31876ccd51ff2c783e37efb33dfa70b97282c2b
Caleb Jones <73506168+calebj516@users.noreply.github.com>
2023-03-12 17:10:32 -0500

    Ticket Adjustments
    Additional styling and layout adjustments for the various Ticket Views. Also made a couple of changes in the Ticket Controller to fix broken links caused by the Index Action no longer being in use.

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -420,7 +420,7 @@ namespace TheBugTracker.Controllers
             ticket.Archived = true;
             await _ticketService.UpdateTicketAsync(ticket);
 
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction(nameof(AllTickets));
         }
 
         // GET: Tickets/Restore/5
@@ -452,7 +452,7 @@ namespace TheBugTracker.Controllers
             ticket.Archived = false;
             await _ticketService.UpdateTicketAsync(ticket);
 
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction(nameof(AllTickets));
         }
 
         private async Task<bool> TicketExists(int id)
